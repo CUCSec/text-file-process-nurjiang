b56eaf6ac20fdf9c0d42bb2b4f9449aa9a82d6b7
nurjiang <13565706187@163.com>
2019-05-19 13:57:48 +0800

    努努1

---
+++ nunu.py
@@ -0,0 +1,8 @@
+﻿count=0
+with open(r'C:\usr\python\log_files\201811113034M.log',encoding='utf8') as f:
+    for line in f:
+        str1=line.split('：')[2]
+        str2=str1[:12]
+        if str2=='201811113034M':
+            count+=1
+print(count)
